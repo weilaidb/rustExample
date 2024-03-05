613de4cbe4128c24eb9448c06f82b6a399904dcf
yjduoduo <wxjlmr@126.com>
2024-03-05 18:40:27 +0800

    create a helloworld

---
+++ helloworld/target/debug/deps/helloworld.d
@@ -0,0 +1,5 @@
+D:\softdevelopment\code\rustExample\helloworld\target\debug\deps\helloworld.exe: src\main.rs
+
+D:\softdevelopment\code\rustExample\helloworld\target\debug\deps\helloworld.d: src\main.rs
+
+src\main.rs:
---
+++ helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+D:\softdevelopment\code\rustExample\helloworld\target\debug\helloworld.exe: D:\softdevelopment\code\rustExample\helloworld\src\main.rs
