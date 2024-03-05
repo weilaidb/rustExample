e4b8af8c0cca98d2d7c4a08796e215e226ddb2c3
yjduoduo <wxjlmr@126.com>
2024-03-05 18:43:53 +0800

    delete debug

--- helloworld/target/debug/deps/helloworld.d
+++
@@ -1,5 +0,0 @@
-D:\softdevelopment\code\rustExample\helloworld\target\debug\deps\helloworld.exe: src\main.rs
-
-D:\softdevelopment\code\rustExample\helloworld\target\debug\deps\helloworld.d: src\main.rs
-
-src\main.rs:
--- helloworld/target/debug/helloworld.d
+++
@@ -1,1 +0,0 @@
-D:\softdevelopment\code\rustExample\helloworld\target\debug\helloworld.exe: D:\softdevelopment\code\rustExample\helloworld\src\main.rs
